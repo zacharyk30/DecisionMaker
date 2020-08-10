34a16a7fc5c682d2d7c547ed26484f317effe45d
Zachary King <zacharyk30@gmail.com>
2020-08-09 22:23:04 -0500

    Changed text

--- DecisionMaker/Form1.Designer.cs
+++ DecisionMaker/Form1.Designer.cs
@@ -91,7 +91,7 @@
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(2, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(984, 32);
+            this.label1.Size = new System.Drawing.Size(1021, 32);
             this.label1.TabIndex = 5;
             this.label1.Text = resources.GetString("label1.Text");
             // 
